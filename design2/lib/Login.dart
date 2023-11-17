847703798f37e6600b36844a4555413ee68d8ea2
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-18 02:43:19 +0530

    all errors fixed

--- design2/lib/Login.dart
+++ design2/lib/Login.dart
@@ -1,13 +1,15 @@
-// ignore_for_file: prefer_const_constructors, file_names, prefer_const_literals_to_create_immutables, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables, file_names
 
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
+
+bool _obscurePassword = true;
 
 class _LoginPageState extends State<LoginPage> {
   @override
@@ -18,183 +20,204 @@ class _LoginPageState extends State<LoginPage> {
           Icons.arrow_back,
         ),
       ),
-      body: Padding(
-        padding:
-            const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Let's Sign you in.",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "Welcome back",
-              style: TextStyle(
-                fontSize: 28,
-                color: const Color.fromARGB(255, 108, 107, 107),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Let's Sign you in.",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
               ),
-            ),
-            Text(
-              "You've been missed!",
-              style: TextStyle(
-                fontSize: 28,
-                color: const Color.fromARGB(255, 108, 107, 107),
+              SizedBox(
+                height: 15,
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 7, bottom: 5),
-              child: Text(
-                "Username or Email",
-                style: TextStyle(fontWeight: FontWeight.bold),
+              Text(
+                "Welcome back",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: const Color.fromARGB(255, 108, 107, 107),
+                ),
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Enter Username or Email",
-                  hintStyle: TextStyle(
-                      color: const Color.fromARGB(255, 198, 163, 163)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 7, bottom: 5),
-              child: Text(
-                "Password",
-                style: TextStyle(fontWeight: FontWeight.bold),
+              Text(
+                "You've been missed!",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: const Color.fromARGB(255, 108, 107, 107),
+                ),
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 7, bottom: 5),
+                child: Text(
+                  "Username or Email",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter Username or Email",
+                    hintStyle: TextStyle(
+                        color: const Color.fromARGB(255, 198, 163, 163)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 7, bottom: 5),
+                child: Text(
+                  "Password",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextFormField(
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
                   hintText: "Enter Password",
                   hintStyle: TextStyle(
                       color: const Color.fromARGB(255, 198, 163, 163)),
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15))),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(),
-                  ),
-                ),
-                Text("or"),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8.0), // Adjust the padding as needed
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: Color.fromARGB(255, 220, 204,
-                          204), // You can customize the border color
-                      width: 2.0,
+                      borderRadius: BorderRadius.circular(15)),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility
+                          : Icons.visibility_off,
                     ),
-                  ),
-                  child: Image.asset(
-                    "Images/google.png",
-                    height: 40,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8.0), // Adjust the padding as needed
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: Color.fromARGB(255, 220, 204,
-                          204), // You can customize the border color
-                      width: 2.0,
-                    ),
-                  ),
-                  child: Image.asset(
-                    "Images/linkedin.png",
-                    height: 40,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8.0), // Adjust the padding as needed
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: Color.fromARGB(255, 220, 204,
-                          204), // You can customize the border color
-                      width: 2.0,
-                    ),
-                  ),
-                  child: Image.asset(
-                    "Images/facebook.png",
-                    height: 40,
-                  ),
-                ),
-              ],
-            ),
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account ? ",
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 129, 129, 129),
-                  ),
-                ),
-                Text(
-                  "Register",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            SizedBox(
-              height: 60,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(
-                      255, 25, 25, 24), // Set button background color
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(15.0), // Set border radius
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Divider(),
+                    ),
+                  ),
+                  Text("or"),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Divider(),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.all(8.0), // Adjust the padding as needed
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 220, 204,
+                            204), // You can customize the border color
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Image.asset(
+                      "Images/google.png",
+                      height: 40,
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.all(8.0), // Adjust the padding as needed
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 220, 204,
+                            204), // You can customize the border color
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Image.asset(
+                      "Images/linkedin.png",
+                      height: 40,
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.all(8.0), // Adjust the padding as needed
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 220, 204,
+                            204), // You can customize the border color
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Image.asset(
+                      "Images/facebook.png",
+                      height: 40,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account ? ",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 129, 129, 129),
+                    ),
+                  ),
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 25, 25, 24), // Set button background color
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(15.0), // Set border radius
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
