4173fc13f9390ecf6559018c7c167ad0da05821c
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-19 22:30:02 +0530

    design 4 done

---
+++ design4/lib/Login.dart
@@ -0,0 +1,205 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Let's Login to your",
+                style: TextStyle(
+                  letterSpacing: 1,
+                  fontFamily: "KdamThmorPro",
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "account!",
+                style: TextStyle(
+                  fontFamily: "KdamThmorPro",
+                  color: Colors.green,
+                  letterSpacing: 1,
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 5),
+                child: Text(
+                  "Email ID",
+                  style: TextStyle(
+                    fontFamily: "KdamThmorPro",
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  hintStyle: TextStyle(
+                    fontFamily: "KdamThmorPro",
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.green,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 5),
+                child: Text(
+                  "Password",
+                  style: TextStyle(
+                    fontFamily: "KdamThmorPro",
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  hintStyle: TextStyle(
+                    fontFamily: "KdamThmorPro",
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.green,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login Now",
+                    style: TextStyle(
+                      fontFamily: "KdamThmorPro",
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      "Don't Have an account?",
+                      style: TextStyle(
+                        fontFamily: "KdamThmorPro",
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontFamily: "KdamThmorPro", color: Colors.green),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text("Or"),
+                  ),
+                  Expanded(
+                    child: Divider(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "Images/google.png",
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        "Sign Up using Google",
+                        style: TextStyle(
+                          fontFamily: "KdamThmorPro",
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                        ),
+                      )
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.green),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
