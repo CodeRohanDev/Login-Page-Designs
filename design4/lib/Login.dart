963022b63f77aee254566ab284b0b84388abc7ba
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-19 22:35:23 +0530

    design 4 100% completed

--- design4/lib/Login.dart
+++ design4/lib/Login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, sort_child_properties_last, prefer_final_fields
 
 import 'package:flutter/material.dart';
 
